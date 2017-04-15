042b1cb3420c019b8228a740ff695bf54de25987
mykhalychnikcolay <mykhalychnickolay@gmail.com>
2017-04-15 13:23:39 +0300

    some

--- src/main/java/mySpring/InjectRandomInt.java
+++ src/main/java/mySpring/InjectRandomInt.java
@@ -1,8 +1,12 @@
 package mySpring;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by nickolay on 14.04.17.
  */
+@Retention(RetentionPolicy.RUNTIME)
 public @interface InjectRandomInt {
     int min();
     int max();
--- src/main/java/mySpring/ObjectFactory.java
+++ src/main/java/mySpring/ObjectFactory.java
@@ -1,12 +1,14 @@
 package mySpring;
 
 import lombok.SneakyThrows;
+import org.reflections.Reflections;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * Created by nickolay on 06.04.17.
@@ -16,12 +18,18 @@ public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
     private Config config = new JavaConfig();
     private List<ObjectConfigurer> objectConfigurers = new ArrayList<>();
+    private Reflections scanner = new Reflections("mySpring");
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
-
+    @SneakyThrows
     private ObjectFactory() {
+        Set<Class<? extends ObjectConfigurer>> classes = scanner.getSubTypesOf(ObjectConfigurer.class);
+        for (Class<? extends ObjectConfigurer> aClass : classes) {
+            objectConfigurers.add(aClass.newInstance());
+
+        }
     }
 
     @SneakyThrows
